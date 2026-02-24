007d4f532a309533e8427db6ce25448eb48a9287
tatertotcode <elijah.kravets@kellogg.northwestern.edu>
2026-02-24 01:14:37 +0000

    added new and create action for places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,4 +2,16 @@ class PlacesController < ApplicationController
   def index
     @places = Place.all
   end
+
+  def new
+    
+  end
+
+  def create
+    @place = Place.new
+    @place["name"] = params["name"]
+    @place.save
+
+    redirect_to "/places"
+  end
 end
