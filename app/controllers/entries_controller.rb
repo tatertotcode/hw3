825f856fb255fd75b2ac449635a56ba4c38062d3
tatertotcode <elijah.kravets@kellogg.northwestern.edu>
2026-02-24 02:47:42 +0000

    for entries, added new action, form view and create action

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,18 @@
 class EntriesController < ApplicationController
+  def new
+    @place = Place.find_by({ "id" => params["place_id"]})
+  end
+
+  def create
+    @entry = Entry.new
+    @entry["title"] = params["title"]
+    @entry["description"] = params["description"]
+    @entry["occurred_on"] = params["occurred_on"]    
+    @entry["place_id"] = params["place_id"] 
+    @entry.save
+
+    # Redirect back to the specific place's show page
+    redirect_to "/places/#{@entry["place_id"]}"
+  end
+
 end
